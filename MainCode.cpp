7dfa83c3d75e0f95495bcf75cb114eecdf539f28
jacnilZhu <35920141+jacnilZhu@users.noreply.github.com>
2020-11-12 13:54:52 +0300

    Update MainCode.cpp

--- MainCode.cpp
+++ MainCode.cpp
@@ -17,7 +17,7 @@ int main()
 	FractalNetwork mynetwork;// define class object
 	// These two lines are used to run a MLP calculation and generate variables following the sampling distribution;
 	mynetwork.MLPNN("Data.txt", 75.1, 328.4013);// MLP neural network
-    mynetwork.Generate_fitting_variable(1,2,"variable.txt");
+  	mynetwork.Generate_fitting_variable(1,2,"variable.txt");
 	// This line is used to test all stochastic distributions in the paper;
 	mynetwork.TestDifferentDis();
 	cout << "finished" << endl;
